5887f840f4dddc833fcab07fa5f5ac84c4b43a01
Elizaveta <pigtail2001@gmail.com>
2021-03-21 17:52:18 +0300

    spring for big app

---
+++ PersonData (spring)/src/main/java/com/krylova/PeopleInfoApplication.java
@@ -0,0 +1,16 @@
+package com.krylova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PeopleInfoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PeopleInfoApplication.class, args);
+		//Application.launch(JavaFxApp.class, args);
+	}
+
+}
